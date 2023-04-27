ce670573d09a9062f21ad5a3ec3bc05d3a6d4b11
Kurmanbekov Nursultan <nursultan.kurmanbekov@alatoo.edu.kg>
2023-04-27 16:24:19 +0600

    Work with settings

--- djangolibraryporject/djangolibraryporject/settings.py
+++ djangolibraryporject/djangolibraryporject/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'djangolibraryporject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django-library', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
