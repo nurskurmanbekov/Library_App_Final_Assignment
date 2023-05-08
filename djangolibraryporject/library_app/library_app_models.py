ab55fcc5e051e76eef4a861246157b0e8f288320
Kurmanbekov Nursultan <nursultan.kurmanbekov@alatoo.edu.kg>
2023-05-09 00:05:49 +0600

    Work with models

--- djangolibraryporject/library_app/library_app_models.py
+++ djangolibraryporject/library_app/library_app_models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('authors_detail_url_nickname', kwargs={'pk': self.id})
