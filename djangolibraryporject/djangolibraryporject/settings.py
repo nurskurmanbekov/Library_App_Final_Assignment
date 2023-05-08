03d98a6981ece88d9b29cd1abbf3735901a961d1
Kurmanbekov Nursultan <nursultan.kurmanbekov@alatoo.edu.kg>
2023-05-08 23:51:16 +0600

    Work with Settings

--- djangolibraryporject/djangolibraryporject/settings.py
+++ djangolibraryporject/djangolibraryporject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'library_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,10 +76,13 @@ WSGI_APPLICATION = 'djangolibraryporject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_library',
+        'USER': 'nurs',
+        'PASSWORD': '556055',
+    } 
 }
+
 
 
 # Password validation
